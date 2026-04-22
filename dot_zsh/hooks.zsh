5537f7a2630410e0a0ad1f625776248b842e81a2
GabrielxD <gabrielxduo@outlook.com>
2026-04-22 18:20:12 +0800

    feat(hooks): remove unused chpwd hook for nvm

--- dot_zsh/hooks.zsh
+++ dot_zsh/hooks.zsh
@@ -1,16 +1,6 @@
 autoload -Uz add-zsh-hook
 
 # ============================================================================ #
-# region Change directory hook
-# ============================================================================ #
-_chpwd_nvm_hook() {
-  [[ -f .nvmrc ]] && nvm use
-}
-
-add-zsh-hook chpwd _chpwd_nvm_hook
-# endregion
-
-# ============================================================================ #
 # region Reload history before each command (make sure aichat save_shell_history works)
 # ============================================================================ #
 _reload_hist() {

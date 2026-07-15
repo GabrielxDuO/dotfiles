7dbb43b1d892d4d5cc6a0af5e2740fa2a19179e0
GabrielxD <gabrielxduo@outlook.com>
2026-07-15 21:05:15 +0800

    fix: make neovim bg transparent in ghostty

--- dot_config/nvim/lua/plugins/catppuccin.lua
+++ dot_config/nvim/lua/plugins/catppuccin.lua
@@ -2,7 +2,7 @@ return {
   {
     "catppuccin/nvim",
     name = "catppuccin",
-    lazy = true,
+    lazy = false,
     priority = 1000,
     opts = {
       -- configurations
@@ -13,7 +13,7 @@ return {
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin",
+      colorscheme = "catppuccin-mocha",
     },
   },
 }

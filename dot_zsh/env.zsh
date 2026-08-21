931382181481e6a11920f6f233c1740a92e343c0
gabxiao <gabxiao@tencent.com>
2026-08-21 16:45:21 +0800

    feat: use vp to ping global node packages

--- dot_zsh/env.zsh
+++ dot_zsh/env.zsh
@@ -35,7 +35,7 @@ eval "$(mise activate zsh)"
 # region VITE+
 # ============================================================================ #
 # Vite+ bin (https://viteplus.dev)
-[[ -f "$HOME/.vite-plus/env" ]] && . "$HOME/.vite-plus/env"
+. "$HOME/.vite-plus/env"
 # endregion
 
 # ============================================================================ #
